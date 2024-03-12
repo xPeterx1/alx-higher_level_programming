20d09b7c4c12bc9467fbbee5c037a0d5a8a2b5df
PETER <ceopeter2003@gmail.com>
2024-03-12 04:31:35 +0200

    not me

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-str = str[str.find("object"):str.rindex("language")] + " with Python"
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
