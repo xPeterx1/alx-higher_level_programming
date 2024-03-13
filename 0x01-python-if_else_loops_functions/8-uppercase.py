e4f7a5b23dcc369f6c2616ddadb5e4a31feae5dc
PETER <ceopeter2003@gmail.com>
2024-03-13 19:24:17 +0200

    TASK 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 def uppercase(str):
-    a = ord('a')
-    z = ord('z')
-    for b in str:
-        if ord(b) in range(a, z + 1):
-            char = chr(ord(b) - 32)
+    A = ord('A')
+    Z = ord('Z') + 1
+    s1 = ord('1')
+    s2 = ord('9') + 1
+    upperconv = ord('a') - ord('A')
+    newstring = []
+    i = 0
+    for c in str:
+        if ord(c) in range(A, Z) or ord(c) in range(s1, s2) or c == " ":
+            newstring.append(c)
         else:
-            char = b
-        print("{:s}".format(char), end="")
-    print('')
+            i = ord(c) - upperconv
+            newstring.append("{:c}".format(i))
+    new_string = "".join(newstring)
+    print(new_string)
