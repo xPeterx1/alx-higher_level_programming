f9992e5d3067a087215775cea88f8a23d7bc074c
PETER <ceopeter2003@gmail.com>
2024-03-13 22:22:44 +0200

    TASK 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for b in range(0, 100):
-    if b == 99:
-        print(b)
+for num in range(0, 100):
+    if num < 10:
+        print("0{}".format(num), end=", ")
     else:
-        print("{:0>2d}".format(b), end=", ")
+        print("{}".format(num), end=", ")
+print()
