20da27e92e3da17314c5565735dd63c46a9ab3ce
PETER <ceopeter2003@gmail.com>
2024-03-15 04:09:20 +0200

    TASK 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg = 0
+    args = sys.argv[1:]
+    lenght = len(args)
+    if (lenght >= 1):
+        print("{} arguments:".format(lenght))
+        for i in args:
+            print("{}: {}".format(arg + 1, args[arg]))
+            arg += 1
+    else:
+        print("0 arguments.")
