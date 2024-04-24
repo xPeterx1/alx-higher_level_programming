54c7278f0991bc5091052253bd604530cc50e948
root <root@LAPTOP-KOLD1Q7N>
2024-04-24 08:45:33 +0200

    TASK 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
             print(x, end="")
         print()
         return x
-    except:
+    except Exception:
         for x in my_list:
             print(x, end="")
         print()
