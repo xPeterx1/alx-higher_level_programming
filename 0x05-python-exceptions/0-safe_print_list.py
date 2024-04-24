4d4b0f21a1ab99305cb982f11bf586bd4c56bb49
root <root@LAPTOP-KOLD1Q7N>
2024-04-24 08:51:59 +0200

    HA

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
             print(x, end="")
         print()
         return x
-    except Exception:
+    except IndexError:
         for x in my_list:
             print(x, end="")
         print()
