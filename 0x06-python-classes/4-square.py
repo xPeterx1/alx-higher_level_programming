1dbc18a004f2a03d0967bc0dbb52cc32cba158f0
root <root@LAPTOP-KOLD1Q7N>
2024-08-01 05:33:00 +0300

    task 3

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,10 +9,19 @@ class Square:
         if isinstance(size, int):
             if (size < 0):
                 raise ValueError('size must be >= 0')
-            self.__size = size
+            self.size = size
         else:
             raise TypeError('size must be an integer')
 
     def area(self):
         """AREA"""
         return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
