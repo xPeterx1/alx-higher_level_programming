8da4575318408088b67ba14d7ec5cca328c07033
root <root@LAPTOP-KOLD1Q7N>
2024-08-04 10:01:21 +0300

    t1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""el7alazona"""
+
+
+class MyList(list):
+    """yma el7alazona"""
+    def print_sorted(self):
+        """el7alazona kant nayma"""
+        x = sorted(self)
+        if x:
+            print(x)
