195a388c9fd2d3314f73e67286ef196d6e0afc8f
root <root@LAPTOP-KOLD1Q7N>
2024-08-07 10:57:06 +0300

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const arg = process.argv?.slice(2);
-if (Number(arg[0])) {
-  const modarg = Math.floor(arg[0]);
+const x = process.argv?.slice(2);
+if (Number(x[0])) {
+  const modarg = Math.floor(x[0]);
   process.stdout.write(('c is fun\n').repeat(modarg));
 } else {
   console.log('Missing number of occurrences');
