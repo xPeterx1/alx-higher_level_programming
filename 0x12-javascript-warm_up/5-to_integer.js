b1a281c84954d0e18c0e53823bf0d69df4a42924
root <root@LAPTOP-KOLD1Q7N>
2024-08-07 10:41:29 +0300

    5-

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
 
+const arg = process.argv?.slice(2);
+
+if (Number(arg[0])) {
+  const modarg = Math.floor(arg[0]);
+  console.log('My number: ' + modarg);
+} else {
+  console.log('Not a number');
+}
