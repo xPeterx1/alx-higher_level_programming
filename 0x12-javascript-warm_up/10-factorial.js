742003255400268895123918f5eeff07538c98b0
root <root@LAPTOP-KOLD1Q7N>
2024-08-08 02:14:52 +0300

    123

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
-
+const args = process.argv?.slice(2);
+function factorial (x) {
+  if (!x) {
+    return 1;
+  }
+  if (x === 1) {
+    return (1);
+  }
+  return (x * factorial(x - 1));
+}
+console.log(factorial(args[0]));
