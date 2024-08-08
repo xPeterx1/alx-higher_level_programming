93b9168e5d4c0e1136688cd5174cc02570685235
root <root@LAPTOP-KOLD1Q7N>
2024-08-09 00:04:13 +0300

    t3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,8 +11,9 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        console.log('X');
+        process.stdout.write('X');
       }
+      console.log('');
     }
   }
 }
