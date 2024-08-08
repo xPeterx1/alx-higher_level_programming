4badc2609fe64db20baf3ef7a3387ac92c527c95
root <root@LAPTOP-KOLD1Q7N>
2024-08-09 00:49:06 +0300

    yes

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    this.tmp = this.width;
+    this.width = this.height;
+    this.height = this.tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = { Rectangle, Square };
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,52 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+      this.index = 0;
+      this.array = ['X', 'C']
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(array[index]);
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    this.tmp = this.width;
+    this.width = this.height;
+    this.height = this.tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+class Square extends Rectangle {
+
+ charPrint(c) {
+
+  if (!(c)) {
+    super.print();
+}
+  else {
+    this.index++;
+    super.print();
+
+  }
+
+
+
+
+}
+}
+
+module.exports = {Rectangle, Square};
