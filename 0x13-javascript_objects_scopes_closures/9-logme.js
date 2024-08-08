a0dcaa4613ba3ac7935b75ff06f72f92d7eee460
root <root@LAPTOP-KOLD1Q7N>
2024-08-09 02:51:48 +0300

    t9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-exports.logMe = function () {
+exports.logMe = (function () {
   let counter = 0;
   function printable (item) {
     console.log(counter + ': ' + item);
     counter++;
   }
   return printable;
-}();
+}());
