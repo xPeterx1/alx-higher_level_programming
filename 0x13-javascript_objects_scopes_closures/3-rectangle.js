7df1e42c02306256abe4afcea7ed8a2e4ba86305
root <root@LAPTOP-KOLD1Q7N>
2024-08-09 01:53:10 +0300

    3344

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,17 @@
-#!/usr/bin/node
-
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+      this.index = 0;
+      this.array = ['X', 'C'];
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
+        process.stdout.write(this.array[this.index]);
       }
       console.log('');
     }
